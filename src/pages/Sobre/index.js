cebe6774752819c41a46849a2a0d7c7ddbbded2a
izabelatrc <izabela.coimbra@fatec.sp.gov.br>
2024-04-14 20:40:34 -0300

    inclusão página sobre

---
+++ src/pages/Sobre/index.js
@@ -0,0 +1,26 @@
+import './sobre.css';
+function Sobre() {
+    return (
+        <div>
+            <h1>Sobre o Site</h1>
+            <p>
+                O site "PrimeFlix" é um projeto desenvolvido como parte do método de avaliação do curso de DSM 3º semestre da Fatec Franca.<br></br>
+                Foi utilizada a API do TMDB (https://www.themoviedb.org/) para fornecer os dados atualizados.<br></br>
+                O site foi implementado para:<br></br>
+                <ul>
+                    <li>Buscar por filmes;</li>
+                    <li>Listar os filmes EM CARTAZ;</li>
+                    <li>Listar os filmes que serão lançados EM BREVE;</li>
+                    <li>Listar os filmes MAIS POPULARES;</li>
+                    <li>Listar os filmes MELHORES AVALIADOS;</li>
+                    <li>Criar uma LISTA com seus filmes FAVORITOS;</li>
+                    <li>Ver SINOPSE de cada filme;</li>
+                    <li>Redirecionar para o TRAILER do filme.</li>
+                </ul>   
+                OBS.: a "minha lista" utiliza o localhoast do navegador para armazenar os dados.
+            </p>
+        </div>
+    );
+}
+
+export default Sobre;
