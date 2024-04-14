d3c6f9257422ff1bb9e1e1e4312eda97f6181dfc
izabelatrc <izabela.coimbra@fatec.sp.gov.br>
2024-04-14 01:20:31 -0300

    Trabalho 3 - PrimeFlix

--- primeflix/src/services/api.js
+++ primeflix/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //URL API: https://api.themoviedb.org/3/movie/now_playing?api_key=817d12607bc5e2e53f4382eceb8654b1&language=pt-br
 
 const api = axios.create({
-    baseURL: 'https://api.themoviedb.org/3'
+    baseURL: 'https://api.themoviedb.org/3/',
 });
 
 export default api;
