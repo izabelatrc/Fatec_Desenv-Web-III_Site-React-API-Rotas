0365eb28951af4a9ceb521c0057b6ad4682d5574
izabelatrc <izabela.coimbra@fatec.sp.gov.br>
2024-04-14 21:19:39 -0300

    merge

--- primeflix/src/routes.js
+++ primeflix/src/routes.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Filme from './pages/Filme';
 import Error from './pages/Error';
 import Favoritos from './pages/Favoritos';
+import Sobre from './pages/Sobre';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -17,6 +18,7 @@ function RoutesApp() {
                 <Route path="/" element={<Home />} />
                 <Route path="/filme/:id" element={<Filme />} />
                 <Route path="/favoritos" element={<Favoritos />} />
+                <Route path="/sobre" element={<Sobre />} />
                 <Route path="*" element={<Error />} />
             </Routes>
             <Footer />
